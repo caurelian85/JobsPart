4148d6d5103a102b14f54f96733d27162447eb6e
Aurelian CD <caurelian85@users.noreply.github.com>
2023-01-27 10:38:16 +0200

    try 2

--- Resume.tsx
+++ Resume.tsx
@@ -262,6 +262,31 @@ export const Resume: React.FC = () => {
           <Section id="langs" header="Languages">
             <BulletedList bullets={["English", "French", "Romanian"]} />
           </Section>
+
+          <Section id="articles" header="Articles">
+            <BulletedList
+              bullets={[
+                {
+                label: "Python Data Structure",
+                href: "https://www.coursera.org/account/accomplishments/verify/52DEXUCAGDB9"
+                },
+                {
+                label: "Programming for Everybody (Getting Started with Python)",
+                href: "https://www.coursera.org/account/accomplishments/verify/K7LYLPSCM8K5"
+                },
+                {
+                  label: "Practitioner of NLP"
+                },
+                {
+                  label: "Algorithms: Design and Analysis, Part 1",
+                  href: "https://github.com/caurelian85/Resume-Docs/blob/master/Pdf/Design%20and%20Analysis%20Part%20I%20_%20Aurelian%20C-D.pdf",
+                },
+                {
+                  label: "Driving license class B"
+                }
+              ]}
+            />
+          </Section>
         </div>
       </body>
     </html>
