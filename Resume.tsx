50b9e035d5957e53fc018bd1992f30c099efcee9
Aurelian CD <caurelian85@users.noreply.github.com>
2023-01-27 10:52:24 +0200

    try 5

--- Resume.tsx
+++ Resume.tsx
@@ -275,14 +275,16 @@ export const Resume: React.FC = () => {
                   href: "https://www.coursera.org/account/accomplishments/verify/K7LYLPSCM8K5"
                 },
                 {
-                  label: "Practitioner of NLP"
+                  label: "Practitioner of NLP",
+                  href: ""
                 },
                 {
                   label: "Algorithms: Design and Analysis, Part 1",
                   href: "https://github.com/caurelian85/Resume-Docs/blob/master/Pdf/Design%20and%20Analysis%20Part%20I%20_%20Aurelian%20C-D.pdf",
                 },
                 {
-                  label: "Driving license class B"
+                  label: "Driving license class B",
+                  href: ""
                 },
               ]}
             />
