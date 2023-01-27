a4cae8984d6771f2f0fe2f04be58c6365d646bd2
Aurelian CD <caurelian85@users.noreply.github.com>
2023-01-27 10:49:56 +0200

    try 4

--- Resume.tsx
+++ Resume.tsx
@@ -267,8 +267,22 @@ export const Resume: React.FC = () => {
             <BulletedList
               bullets={[
                 {
-                  label: "Build your resume in React + SSG!",
-                  href: "https://dev.to/jeckhummer/build-your-resume-in-react-ssg-10i4",
+                  label: "Python Data Structure",
+                  href: "https://www.coursera.org/account/accomplishments/verify/52DEXUCAGDB9"
+                },
+                {
+                  label: "Programming for Everybody (Getting Started with Python)",
+                  href: "https://www.coursera.org/account/accomplishments/verify/K7LYLPSCM8K5"
+                },
+                {
+                  label: "Practitioner of NLP"
+                },
+                {
+                  label: "Algorithms: Design and Analysis, Part 1",
+                  href: "https://github.com/caurelian85/Resume-Docs/blob/master/Pdf/Design%20and%20Analysis%20Part%20I%20_%20Aurelian%20C-D.pdf",
+                },
+                {
+                  label: "Driving license class B"
                 },
               ]}
             />
